a62ca00b3d990f3ec5c699ea0c905a0a441dacad
hyoya <ghtybndss@naver.com>
2020-05-24 02:56:25 +0900

    toc minimumHeaders set 1

--- assets/js/toc.js
+++ assets/js/toc.js
@@ -4,7 +4,7 @@
     var defaults = {
       noBackToTopLinks: false,
       title: '',
-      minimumHeaders: 3,
+      minimumHeaders: 1,
       headers: 'h2, h3, h4, h5, h6',
       listType: 'ol', // values: [ol|ul]
       showEffect: 'show', // values: [show|slideDown|fadeIn|none]
